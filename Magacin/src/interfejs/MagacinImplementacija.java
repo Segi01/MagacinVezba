6df624c944d08b83ec8b92aee70cacbb52a50693
stefan.segrt.5@gmail.com <Segi01>
2024-03-26 22:54:03 +0100

    Uvedena klasa koja implementira InterfejsMagacin

---
+++ Magacin/src/interfejs/MagacinImplementacija.java
@@ -0,0 +1,25 @@
+package interfejs;
+
+import magacin.Artikal;
+
+public class MagacinImplementacija implements IntefejsMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
