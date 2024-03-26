32551dc6d6dd1ac14b2831dc8c6f2ae2e6fc479d
stefan.segrt.5@gmail.com <Segi01>
2024-03-26 22:40:53 +0100

    Uveden interfejs interfejsMagacin sa metodama dodaj,izbaci i
     pronaji
    artikal

---
+++ Magacin/src/interfejs/IntefejsMagacin.java
@@ -0,0 +1,13 @@
+package interfejs;
+
+import magacin.Artikal;
+
+public interface IntefejsMagacin {
+	
+	public void dodajArtikal(Artikal artikal);
+	
+	public void izbaciArtikal(Artikal artikal);
+	
+	public Artikal pronadjiArtikal(int sifra);
+
+}
