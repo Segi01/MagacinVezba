d460fdf2396b5ffcb1b64a1e5c165118e13571fc
stefan.segrt.5@gmail.com <Segi01>
2024-03-26 23:17:21 +0100

    Implementira klasa na drugi nacin

--- Magacin/src/interfejs/MagacinImplementacija.java
+++ Magacin/src/interfejs/MagacinImplementacija.java
@@ -1,25 +1,52 @@
 package interfejs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import magacin.Artikal;
 
 public class MagacinImplementacija implements IntefejsMagacin {
 
+	
+	List<Artikal> artikli=new ArrayList<>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		
+		for (Artikal a : artikli) {
+						 
+						a.setKolicina(a.getKolicina()+artikal.getKolicina());
+						
+						
+					}
+					artikli.add(artikal);
 		
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal==null)
+			throw new NullPointerException("Artikal ne sme biti null");
+		if(artikli.size()==0)
+			
+		artikli.remove(artikal);
+		if(artikli.contains(artikal))
+			throw new IllegalArgumentException("Lista artikala je prazna");
+			
+		
 		
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		for (Artikal a : artikli) {
+					if(a.getSifra()==sifra) {
+						throw new IllegalArgumentException("Artikal ne postoji");
+												}
+				}
+		
 		return null;
+					
 	}
 
 }
